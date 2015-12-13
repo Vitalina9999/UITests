d5647984f6a86dd52cd2617bc4ef41a167b6ea2a
Sergey Boiko <sergey.boiko4@gmail.com>
2015-12-13 01:38:52 +0100

    added base structure

---
+++ ParkingCalculator.TestModel/PageDeclarations/HomePage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace ParkingCalculator.TestModel.PageDeclarations
+{
+    public class HomePage : PageBase
+    {
+        #region Properties
+        [FindsBy(How = How.XPath, Using = @"id(""wpName2"")")]
+        protected IWebElement txtUserName { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = @"id(""wpPassword2"")")]
+        protected IWebElement txtPassword { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = @"id(""wpRetype"")")]
+        protected IWebElement txtConfirmPassword { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = @"id(""wpEmail"")")]
+        protected IWebElement txtEmailAddress { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = @"id(""wpCaptchaWord"")")]
+        protected IWebElement txtCaptcha { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = @"id(""wpCreateaccount"")")]
+        protected IWebElement btnCreateYourAccount { get; set; }
+        #endregion
+
+        public override void Invoke()
+        {
+            Driver.Url = @"https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page&type=signup";
+        }
+        public override bool IsDisplayed()
+        {
+            return txtCaptcha.Displayed;
+        }
+        public override void VerifyExpectedElementsAreDisplayed()
+        {
+            VerifyElementVisible("txtUserName", txtUserName);
+            VerifyElementVisible("txtPassword", txtPassword);
+            VerifyElementVisible("txtConfirmPassword", txtConfirmPassword);
+            VerifyElementVisible("txtEmailAddress", txtEmailAddress);
+            VerifyElementVisible("txtCaptcha", txtCaptcha);
+            VerifyElementVisible("btnCreateYourAccount", btnCreateYourAccount);
+        }
+    }
+}
---
+++ ParkingCalculator.Tests/Smoke/SmokeTestsForEachPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingCalculator.TestModel;
+
+namespace ParkingCalculator.Tests.Smoke
+{
+    [TestClass]
+    public class SmokeTestsForEachPage : TestBase
+    {
+        public void PageTest(PageBase page)
+        {
+            // Implement Dispose inside page object in order to do cleanup
+            using (page)
+            {
+                // SwdBrowser.HandleJavaScriptErrors();
+
+                page.Invoke();
+
+                // SwdBrowser.HandleJavaScriptErrors();
+
+                page.VerifyExpectedElementsAreDisplayed();
+
+                // SwdBrowser.HandleJavaScriptErrors();
+            }
+        }
+
+        // Add testMethods for your new pages here:
+        // *PageName*_VerifyExpectedElements()
+
+        [TestMethod]
+        public void HomePage_VerifyExpectedElements()
+        {
+            PageTest(Pages.HomePage);
+        }
+
+        //[TestMethod]
+        //public void CreateNewAccountPage_VerifyExpectedElements()
+        //{
+        //    PageTest(Pages.HomePage);
+        //}
+    }
+}
